33a1bb84e74b52d7313d0532c2c84dd5021da425
jrodri30 <jason.rodriguez@dca.ga.gov>
2014-04-22 18:47:11 -0400

    Update Gender app
    Minor bug fixes

--- Demographics/Gender/ui.R
+++ Demographics/Gender/ui.R
@@ -1,52 +1,100 @@
+#.libPaths("~/R/win-library/3.0")
+#install.packages("RJDBC",repos="http://cran.rstudio.com/")
+
 # Load shiny and RODBC packages
 library(shinyIncubator)
 library(RJDBC)
 
 
 # Establish JDBC connection using RJDBC
-drv <- JDBC("oracle.jdbc.OracleDriver",classPath="../../lib/ojdbc6.jar", " ")
-source("~/HMIS Data Analyst/lib/connectionkey.r",local=TRUE)
+source("../connectionkey.r",local=TRUE)
 
-# Define UI for HMIS gender trends application
 shinyUI(basicPage(
-  #Initiate progress indicators
   progressInit(),
+  br(),
   fluidRow(
-    # SIDE PANEL
     column(4,
-      wellPanel(
-        h3("Gender",align="center"),
-        # Tab panels within side panel
+      tags$form(class="well noprint",
+        h3("Housing Destinations",align="center"),
         tabsetPanel(selected="Data Options",
           tabPanel("Instructions",
             p('1. FIRST select your data options.'),
             p('2. Click the "Analyze" button.'),
-            p('3. When the analysis is complete, the Main Panel will update.')
+            p('3. When the analysis is complete, the Main Panel will update. Upon completion, you will
+               also be able to download your data as a spreadsheet.'),
+            p('4. If you are not satisfied with the appearance of the report, 
+               you can modify the viewing options.')
           ),
           tabPanel("Data Options",
-            # Require user to click "ANALYZE" button in order for graph to update
             div(actionButton("update",strong("ANALYZE"),icon=icon("arrow-circle-right")),align="right"),
-            # Import "Data Options" ui code
-            source("../../lib/Data Options.ui.r",local=TRUE)
+            # Display date range input with default values
+            dateRangeInput("daterange","Date range", format='mm/dd/yyyy',start="2013-07-01",max=Sys.Date()),
+            br(),
+            # Display radio buttons with 3 defined report levels
+            radioButtons("reportLevel", "Report level",list("Group","Agency","Program"),selected="Program"),
+            br(),
+            # Display "Group Name" select list when user chooses "Group" report level
+            conditionalPanel(
+              # Define condition of this display
+              condition = "input.reportLevel == 'Group'",
+              # Query Pathways group names to populate select list
+              selectInput("groupSelect", "Group name",as.list(as.character(
+                dbGetQuery(connection,"
+                  SELECT unique Group_Name 
+                  FROM Community_Group_Information
+                  ORDER BY Group_Name"
+                )[[1]]
+              )))
+            ),
+            # Display "Agency Name" select list when user chooses "Agency" or "Program" report levels
+            conditionalPanel(
+              # Define condition of this display
+              condition = "input.reportLevel == 'Agency' | input.reportLevel == 'Program'",
+              # Query agency names to populate select list
+              selectInput("agencySelect", "Agency name",as.list(as.character(
+                dbGetQuery(connection,"
+                  SELECT unique Agency_Name 
+                  FROM Program_Profile_Info
+                  ORDER BY Agency_Name"
+                )[[1]]
+              )))
+            ),
+            # Display "Program Name" select list when user chooses "Program" report level
+            conditionalPanel(
+              # Define condition of this display
+              condition = "input.reportLevel == 'Program'",
+              # Call "programChoices" (reactive select list, defined in server.R)
+              # Display on sidebar
+              uiOutput("programChoices")
+            ),
+            br(),
+            uiOutput("programTypes"),
+            br(),br(),br(),br(),br(),br(), br()
+          ),
+          tabPanel("Viewing Options",
+            checkboxInput("printable", "Printable", FALSE),
+            checkboxInput("mdkr", 'Omit unknown destinations (missing, "Don\'t know," and "Refused")',FALSE)
           ),
           tabPanel("About",
-            p(strong("Title:"),"Georgia HMIS Demographics - Gender"),
+            p(strong("Title:"),"Georgia HMIS Housing Destinations"),
             p(strong("Version:"),"2.0.0"),
-            p(strong("Date:"),"21 April 2014"),
+            p(strong("Date:"),"9 April 2014"),
+            p(strong("Author:"),"Jason Rodriguez (Georgia Department of Community Affairs)"),
             p(strong("Description:"),"Set of customizable reports and charts for the purpose of reporting
-              gender demographic trends among homeless service providers in the state of Georgia"),
-            p(strong("Bug reports:"),"Send to ",a(href="mailto:jsn.rgz@gmail.com","jsn.rgz@gmail.com")),
-            p(div(strong("Source code:"),"View on",a(href="https://github.com/hmis-analyst/shiny-apps-oracle/tree/master/Demographics/Gender","GitHub"))),
+              and improving program outcomes among homeless service providers in the state of Georgia"),
+            p(strong("Bug reports:"),"Send to ",a(href="mailto:jason.rodriguez@dca.ga.gov","jason.rodriguez@dca.ga.gov")),
+            p(strong("Source code:"),a(href="https://github.com/jrodri30/HMIS/tree/master/Housing Outcomes",
+              "https://github.com/jrodri30/HMIS/tree/master/Housing Outcomes")
+            ),
             p(strong('Changes since last version:')),
-            p(' - Upgraded to new page layout and data options')
+            p(' - Overhauled to look more like Data Quality app'),
+            p(' - Minor bug fixes')
           )
         )
       )
     ),
-    # MAIN PANEL
     column(8,
-      # Call "Plot" (reactive plot, defined in server.R)
-      plotOutput("Plot")
+      uiOutput("mainPanel")
     )
   )
 ))
