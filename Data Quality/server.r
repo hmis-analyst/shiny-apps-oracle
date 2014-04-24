45b9fb6a4fb64fe2108cc2e209e8d29fee96ffa5
Jason Rodriguez <jsn.rgz@gmail.com>
2014-04-24 16:04:21 -0400

    Update server.r

--- Data Quality/server.r
+++ Data Quality/server.r
@@ -16,7 +16,7 @@ library(stringr)
 
 
 # Establish JDBC connection using RJDBC
-drv <- JDBC("oracle.jdbc.OracleDriver",classPath="../../lib/ojdbc6.jar", " ")
+drv <- JDBC("oracle.jdbc.OracleDriver",classPath="~/GitHub/shiny-apps-oracle/lib/ojdbc6.jar", " ")
 source("~/HMIS Data Analyst/lib/connectionkey.r",local=TRUE)
 
 # Create a function to modify date format
